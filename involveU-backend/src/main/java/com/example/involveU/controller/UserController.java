a9e611ad4a0632ddd1481fdc81d87995bedb5152
Nicholas LeBoeuf <nicholas.leboeuf@snhu.edu>
2022-09-14 15:57:31 -0400

    Fixed CORS Policy Issue
    Had to add the origin for the localhost URL with port 4200 (the running project port).
    
     We were all having issues with CORS Policy blocking us from using the API port (8080) and not displaying the data.
    
    This line of code trusts port 4200 and allows for cross-origin resource sharing.
    
    https://spring.io/guides/gs/rest-service-cors/

--- involveU-backend/src/main/java/com/example/involveU/controller/UserController.java
+++ involveU-backend/src/main/java/com/example/involveU/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.involveU.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class UserController {
 
 	@Autowired
 	private UserRepository userRepository;
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("users")
 	public List<User> getUsers() {
 		return this.userRepository.findAll();

4e962d507fce67fcd248534c97e8ba5b0e1d8ddd
Ryan Simas <ryan.simas@snhu.edu>
2022-09-27 16:42:15 -0400

    Created eboard.java class
    Created eboard class file

---
+++ involveU-backend/src/main/java/com/example/involveU/model/EBoard.java
@@ -0,0 +1,12 @@
+package com.example.involveU.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import javax.persistence.Id;
+
+public class EBoard {
+
+}

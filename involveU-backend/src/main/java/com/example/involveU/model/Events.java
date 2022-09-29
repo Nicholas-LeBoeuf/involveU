661c138cbc685cc661d77cdada373f3e70fd8051
Ryan Simas <ryan.simas@snhu.edu>
2022-09-29 13:36:13 -0400

    Created setters and getters for events variables
    Generated setters and getters for each variable

--- involveU-backend/src/main/java/com/example/involveU/model/Events.java
+++ involveU-backend/src/main/java/com/example/involveU/model/Events.java
@@ -26,4 +26,40 @@ public class Events {
     private  String ticketLink;
     @Column(name = "clubID")
     private  int clubID;
+
+    public String getEventName() {return eventName;}
+
+    public void setEventName(String eventName) {this.eventName = eventName;}
+
+    public String getEventLocation() {return eventLocation;}
+
+    public void setEventLocation(String eventLocation) {this.eventLocation = eventLocation;}
+
+    public String getStartTime() {return startTime;}
+
+    public void setStartTime(String startTime) {this.startTime = startTime;}
+
+    public String getEndTime() {return endTime;}
+
+    public void setEndTime(String endTime) {this.endTime = endTime;}
+
+    public String getEventDate() {return eventDate;}
+
+    public void setEventDate(String eventDate) {this.eventDate = eventDate;}
+
+    public String getEventDesc() {return eventDesc;}
+
+    public void setEventDesc(String eventDesc) {this.eventDesc = eventDesc;}
+
+    public int getIsTransportation() {return isTransportation;}
+
+    public void setIsTransportation(int isTransportation) {this.isTransportation = isTransportation;}
+
+    public String getTicketLink() {return ticketLink;}
+
+    public void setTicketLink(String ticketLink) {this.ticketLink = ticketLink;}
+
+    public int getClubID() {return clubID;}
+
+    public void setClubID(int clubID) {this.clubID = clubID;}
 }

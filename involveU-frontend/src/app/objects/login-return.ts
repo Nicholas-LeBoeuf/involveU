1564948dcfea17df269047d2b8418a95ae6e547b
Nicholas LeBoeuf <nicholas.leboeuf@snhu.edu>
2022-10-02 16:19:01 -0400

    Updated Frontend Objects
    Updated the frontend objects to be an interface instead of a class.

---
+++ involveU-frontend/src/app/objects/login-return.ts
@@ -0,0 +1,4 @@
+export interface LoginReturn {
+  userID: string;
+  httpResponseMessage: string;
+}
--- involveU-frontend/src/app/objects/user.ts
+++ involveU-frontend/src/app/objects/user.ts
@@ -1,6 +1,6 @@
-export class User {
-  id?: number;
-  firstName?: string;
-  lastName?: string;
-  email?: string;
+export interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
 }

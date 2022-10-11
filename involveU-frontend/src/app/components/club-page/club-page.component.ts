3efdfa8cf5ce3638e76cd9a8e274858a8122cd7b
Ryan Simas <ryan.simas@snhu.edu>
2022-10-11 17:41:26 -0400

    Created Club Component
    Created club component using WebStorm

---
+++ involveU-frontend/src/app/components/club-page/club-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-club-page',
+  templateUrl: './club-page.component.html',
+  styleUrls: ['./club-page.component.scss']
+})
+export class ClubPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

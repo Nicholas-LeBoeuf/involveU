75e980def58955a086aeaa5c8d7a6e870c0899f9
Ryan Simas <ryan.simas@snhu.edu>
2022-10-11 22:01:29 -0400

    Fixed formatting issues from merge

--- involveU-frontend/src/app/app.module.ts
+++ involveU-frontend/src/app/app.module.ts
@@ -3,19 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from "@angular/forms";
-
 import { AppComponent } from './components/app.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { ClubPageComponent } from './components/club-page/club-page.component';
-
 import { DialogModule } from 'primeng/dialog';
 import {ButtonModule} from "primeng/button";
 import {CookieService} from "ngx-cookie-service";
-
-import { ClubPageComponent } from './components/club-page/club-page.component';
 import {RouterOutlet} from "@angular/router";
 import { AppRoutingModule } from './app-routing.module';
-
 import {CarouselModule} from "primeng/carousel";
 
 
@@ -32,12 +27,9 @@ import {CarouselModule} from "primeng/carousel";
     ButtonModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
-
     RouterOutlet,
-    AppRoutingModule
-
+    AppRoutingModule,
     CarouselModule
-
   ],
   providers: [
     CookieService

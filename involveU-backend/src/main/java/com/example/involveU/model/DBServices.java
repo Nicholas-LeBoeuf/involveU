6feaa87bf28d299f0120a9d94d40973830b19351
NicholasGaston <59942769+NicholasGaston@users.noreply.github.com>
2022-11-11 15:26:48 -0500

    Favorite Club Events Fix
    Favorite Club Events returns only future events.

--- involveU-backend/src/main/java/com/example/involveU/model/DBServices.java
+++ involveU-backend/src/main/java/com/example/involveU/model/DBServices.java
@@ -305,7 +305,7 @@ public class DBServices {
     }
     protected  List<Events> getDBFavoriteClubEvents(int userID)
     {
-        sql = "select eventID ,eventName, startTime, eventLocation, endTime, eventDate,eventDesc, isTransportation,ticketLink, Events.clubID  from Events JOIN Favorites ON Events.clubID = Favorites.clubID AND Favorites.userID =" + userID + " ORDER BY eventDate ,startTime ASC;";
+        sql = "select eventID ,eventName, startTime, eventLocation, endTime, eventDate,eventDesc, isTransportation,ticketLink, Events.clubID from Events JOIN Favorites ON eventDate >= DATE(NOW()) AND Events.clubID = Favorites.clubID AND Favorites.userID = "+userID +" ORDER BY eventDate ,startTime ASC;\n";
 
         events = JdbcTemplated.query(sql,BeanPropertyRowMapper.newInstance(Events.class));
 

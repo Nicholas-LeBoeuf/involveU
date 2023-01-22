6c1573f0b89d78a1bc3ab3da87e6242f4d3c32c3
Nicholas LeBoeuf <nicholas.leboeuf@snhu.edu>
2023-01-22 12:47:39 -0500

    Switched over the event data to eboard page

--- involveU-frontend/src/app/components/eboard-page/eboard-page.component.ts
+++ involveU-frontend/src/app/components/eboard-page/eboard-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {ClubService} from "../../services/club.service";
 import {EventsService} from "../../services/events.service";
 import {EboardService} from "../../services/eboard.service";
@@ -8,6 +8,8 @@ import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {Club} from "../../objects/club";
 import {Announcement} from "../../objects/announcements";
 import { DatePipe } from '@angular/common';
+import {Events} from "../../objects/events";
+import {Table} from "primeng/table";
 
 @Component({
   selector: 'app-eboard-page',
@@ -16,10 +18,13 @@ import { DatePipe } from '@angular/common';
 })
 export class EboardPageComponent implements OnInit {
   announcementForm : FormGroup;
+  createEventForm : FormGroup;
+  editEventForm : FormGroup
   todaysDate = new Date().toString();
   constructor(private clubService: ClubService,
               private formBuilder: FormBuilder,
               private eboardService: EboardService,
+              private eventsService: EventsService,
               private route: ActivatedRoute,
               private router: Router,
               public cookie: CookieService,
@@ -31,22 +36,80 @@ export class EboardPageComponent implements OnInit {
       announcementTitle: ['', Validators.required],
       postedOn: ['']
     })
+    this.createEventForm = this.formBuilder.group({
+      createEventName: ['', Validators.required],
+      createEventLocation: ['', Validators.required],
+      createStartTime: ['', Validators.required],
+      createEndTime: ['', Validators.required],
+      createEventDate: ['', Validators.required],
+      createEventDesc: ['', Validators.required],
+      createIsTransportation: ['', Validators.required],
+      createTicketLink: ['', Validators.required],
+
+    })
+    this.editEventForm = this.formBuilder.group({
+      editEventName: ['', Validators.required],
+      editEventLocation: ['', Validators.required],
+      editStartTime: ['', Validators.required],
+      editEndTime: ['', Validators.required],
+      editEventDate: ['', Validators.required],
+      editEventDesc: ['', Validators.required],
+      editIsTransportation: ['', Validators.required],
+      editTicketLink: ['', Validators.required],
+
+    })
     this.todaysDate = this.datePipe.transform(this.todaysDate, 'yyyy-MM-dd');
   }
-  clubID!: number;
-  userID!: number;
-  clubInfo!: Club;
+
+  cols = [
+    { field: 'eventName', header: 'Name' },
+    { field: 'eventDate', header: 'Date' },
+    { field: 'startTime', header: 'Start Time' },
+    { field: 'endTime', header: 'End Time' },
+    { field: 'eventLocation', header: 'Location' },
+    { field: 'eventDesc', header: 'Description' },
+    { field: 'ticketLink', header: 'Ticket Link' },
+    { field: 'isTransportation', header: 'Transportation' }
+  ];
+  @ViewChild('clubEventTable') clubEventTable: Table;
+  clubID: number;
+  userID: number;
+  clubInfo: Club;
+  clubEvents: Events[] = [];
+  certainEvent: Events[] = [];
+  locations: Events[] = [];
+  spaces: Events[] = [];
+  addEventDialog: boolean = false;
+  editDialog: boolean = false;
+  addEventSuccess: boolean = false;
+  addEventFailed: boolean = false;
+  editEventSuccess: boolean = false;
+  editEventFailed: boolean = false;
+  disableUserDropdown = true;
+  selectedLocation: any = {};
+
+  locationID: FormControl = new FormControl(null);
+  spaceID : FormControl = new FormControl(null);
+
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.clubID = params['id'];
     });
     this.userID = +this.cookie.get('studentID');
     this.getClubInfo();
+    this.getClubEvents();
+    this.getSpacesByLocation();
   }
 
   getClubInfo() {
     this.clubService.getSpecificClub(this.clubID).subscribe(response => {
       this.clubInfo = response;
+    })
+  }
+
+  getClubEvents() {
+    this.eventsService.getSpecificClubEvents(this.clubID).subscribe(response => {
+      this.clubEvents = response;
     })
   }
 
@@ -65,5 +128,115 @@ export class EboardPageComponent implements OnInit {
       (error) => {
         console.log(error);
       });
+  }
+
+  onFilterEventName(event: Event) {
+    this.clubEventTable.filterGlobal((event.target as HTMLInputElement).value.toString(), 'contains');
+  }
+
+  showAddEventDialog()
+  {
+    this.addEventDialog = true;
+  }
+  closeAddEventDialog()
+  {
+    this.addEventDialog = false;
+  }
+
+  showEditDialog(SpecficEvent: Events)
+  {
+    this.certainEvent.push(SpecficEvent);
+    this.editDialog = true;
+  }
+  closeEditDialog() {
+    this.certainEvent = [];
+    this.editDialog = false;
+  }
+
+  deleteEvent(eventID: number) {
+    this.eventsService.deleteEvent(eventID).subscribe(response => {
+      console.log(response);
+    })
+    location.reload();
+  }
+
+  get getCreateEventsFormInputs()
+  {
+    return this.createEventForm.controls;
+  }
+
+  getSpacesByLocation() {
+    this.eventsService.getSpaceByLocation(this.locationID.value).subscribe(response => {
+        this.spaces = response;
+      },
+      (error) => {
+        console.log(error)
+      });
+  }
+
+  checkLocationSelected() {
+    if(this.selectedLocation!=='Select Location'){
+      this.disableUserDropdown = false;
+      this.getSpacesByLocation();
+    }
+    else {
+      this.disableUserDropdown = true;
+    }
+  }
+
+  areCreateFormInputsValid()
+  {
+    if(this.createEventForm.value.createEventName == '' || this.createEventForm.value.createEventLocation == '' ||this.createEventForm.value.createStartTime == '' || this.createEventForm.value.createEndTime == '' || this.createEventForm.value.createEventDate == '' || this.createEventForm.value.createEventDesc == '') {
+      return true;
+    }
+    else{
+      return false;
+    }
+
+  }
+
+  submitNewEvent()
+  {
+    const eventInfo : Events = { eventName: this.createEventForm.value.createEventName, eventLocation: this.createEventForm.value.createEventLocation, startTime: this.createEventForm.value.createStartTime, endTime: this.createEventForm.value.createEndTime, eventDate: this.createEventForm.value.createEventDate, eventDesc: this.createEventForm.value.createEventDesc, isTransportation: this.createEventForm.value.createIsTransportation, ticketLink: this.createEventForm.value.createTicketLink, clubName:  this.clubInfo.clubName, clubID: this.clubInfo.clubID };
+
+    this.eventsService.submitNewEvent(eventInfo).subscribe(success =>{
+      console.log(success);
+      this.addEventSuccess = true;
+      location.reload();
+    },(error) =>{
+      location.reload();
+      console.log(error.text);
+      this.addEventFailed = true;
+    })
+  }
+
+  updateEvent()
+  {
+    const eventInfo : Events = {eventID: this.certainEvent[0].eventID, eventName: this.editEventForm.value.editEventName,eventLocation: this.editEventForm.value.editEventLocation, startTime: this.editEventForm.value.editStartTime, endTime: this.editEventForm.value.editEndTime, eventDate: this.editEventForm.value.editEventDate, eventDesc: this.editEventForm.value.editEventDesc, isTransportation: this.editEventForm.value.editIsTransportation, ticketLink: this.editEventForm.value.editTicketLink,clubName:  this.clubInfo.clubName, clubID: this.clubInfo.clubID };
+
+    this.eventsService.updateEvent(eventInfo).subscribe(success =>{
+      console.log(success);
+      this.editEventSuccess = true;
+    },(error) =>{
+
+      this.getClubEvents();
+      console.log(error.text);
+      this.editEventFailed = true;
+    })
+  }
+
+  get getEditEventsFormInputs()
+  {
+    return this.editEventForm.controls;
+  }
+  areEditFormInputsValid()
+  {
+    if(this.editEventForm.value.editEventName == '' || this.editEventForm.value.editEventLocation == '' ||this.editEventForm.value.editStartTime == '' || this.editEventForm.value.editEndTime == '' || this.editEventForm.value.editEventDate == '' || this.editEventForm.value.editEventDesc == '') {
+      return true;
+    }
+    else{
+      return false;
+    }
+
   }
 }

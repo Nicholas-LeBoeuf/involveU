8bc0e47367962208e477b3d31435efda9bfb10b5
Ryan Simas <ryan.simas@snhu.edu>
2023-02-18 23:30:12 -0500

    Set More Info Page title

--- involveU-frontend/src/app/components/more-info/more-info.component.ts
+++ involveU-frontend/src/app/components/more-info/more-info.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ResponsiveService} from "../../services/responsive.service";
+import {Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-more-info',
@@ -8,7 +9,10 @@ import {ResponsiveService} from "../../services/responsive.service";
 })
 export class MoreInfoComponent implements OnInit {
 
-  constructor(public responsiveService: ResponsiveService) { }
+  constructor(public responsiveService: ResponsiveService,
+              private title: Title) {
+    this.title.setTitle("involveU | More Information")
+  }
 
   ngOnInit(): void {
   }

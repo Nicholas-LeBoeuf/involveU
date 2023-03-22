d76b0462e4927ad3d5a950e247f264c412b053f7
Ryan Simas <ryan.simas@snhu.edu>
2023-03-22 16:11:29 -0400

    Added Email class

---
+++ involveU-backend/src/main/java/com/example/involveU/model/EmailDetails.java
@@ -0,0 +1,20 @@
+package com.example.involveU.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// Annotations
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+// Class
+public class EmailDetails {
+
+    // Class data members
+    private String recipient;
+    private String msgBody;
+    private String subject;
+    private String attachment;
+}

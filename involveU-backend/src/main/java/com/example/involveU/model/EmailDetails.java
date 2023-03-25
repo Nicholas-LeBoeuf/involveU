0e4c702a8ac4d96d8521f68c1209028601faad4b
Nicholas LeBoeuf <nicholas.leboeuf@snhu.edu>
2023-03-25 16:20:03 -0400

    Fixed bug where the new button hover would blend in with blue nav bar or title banner

--- involveU-backend/src/main/java/com/example/involveU/model/EmailDetails.java
+++ involveU-backend/src/main/java/com/example/involveU/model/EmailDetails.java
@@ -1,14 +1,5 @@
 package com.example.involveU.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-// Annotations
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-
 // Class
 public class EmailDetails {
 
@@ -16,4 +7,16 @@ public class EmailDetails {
     private String recipient;
     private String msgBody;
     private String subject;
+
+    public String getMsgBody() {
+        return msgBody;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
 }

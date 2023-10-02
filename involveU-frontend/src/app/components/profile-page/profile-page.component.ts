42a43530f521c793cf4bff70b9d1b9119be664f6
amraibra <92942609+amraibra@users.noreply.github.com>
2023-10-02 14:38:35 -0400

    Profile information skeleton
    Basic set up of profile page with user's name linked to the page

--- involveU-frontend/src/app/components/profile-page/profile-page.component.ts
+++ involveU-frontend/src/app/components/profile-page/profile-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {CookieService} from "ngx-cookie-service";
+import {User} from "../../objects/user";
 
 @Component({
   selector: 'app-profile-page',
@@ -13,13 +14,21 @@ export class ProfilePageComponent implements OnInit {
   ngOnInit(): void {
     this.userID = +this.cookie.get('studentID');
     this.isUserLoggedIn();
+    this.getUserInfo();
   }
 
   isLoggedIn: boolean = false;
   userID: number;
+  currentUser: User;
 
   isUserLoggedIn() {
     this.isLoggedIn = this.userID !== 0;
+  }
+
+  getUserInfo() {
+    this.currentUser = {studentID: +this.cookie.get('studentID'), firstName: this.cookie.get('studentFName'), lastName: this.cookie.get('studentLName')};
+    this.currentUser.firstName = this.currentUser.firstName.replace(/['"]/g, '');
+    this.currentUser.lastName = this.currentUser.lastName.replace(/['"]/g, '');
   }
 
 }

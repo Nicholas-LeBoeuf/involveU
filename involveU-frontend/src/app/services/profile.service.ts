10894c0c3e8adac12e1c5eaa4e8de8fb118f3f59
PaulAntuono <49251120+PaulAntuono@users.noreply.github.com>
2023-10-06 14:33:31 -0400

    Updating the profile service

--- involveU-frontend/src/app/services/profile.service.ts
+++ involveU-frontend/src/app/services/profile.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import { Observable } from "rxjs";
+import { User } from '../objects/user'
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
+  getUserProfile(UserID: number): Observable<User[]> {
+    return this.http.get<User[]>(environment.apiURL + `user/GetUserProfile/${UserID}`);
+  }
+
+  changeUserPronouns(UserID: number): Observable<User[]> {
+    return this.http.get<User[]>(environment.apiURL + `user/changePronouns/${UserID}`);
+  }
+
+  checkUserPassword(UserID: number, password?: string): Observable<User> {
+    return this.http.get<User>(environment.apiURL + `user/checkPassword/${UserID}/${password}`);
+  }
 }

b3c401227f9a935fa5da3223a58a58085d8afafc
dude <137600619+dude@users.noreply.github.com>
2023-12-01 14:06:45 -0500

    Improvement to Front end of public profile

--- involveU-frontend/src/app/components/public-profile/public-profile.component.ts
+++ involveU-frontend/src/app/components/public-profile/public-profile.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { ProfileService } from '../../services/profile.service'; // Corrected import
+import {ActivatedRoute, Router} from '@angular/router';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { ProfileService } from '../../services/profile.service';
+import { ClubService } from '../../services/club.service'; // Import the ClubService
+import { Club } from '../../objects/club'; // Assuming you have a Club model
+
 
 @Component({
   selector: 'app-public-profile',
@@ -8,27 +12,30 @@ import { ProfileService } from '../../services/profile.service'; // Corrected im
   styleUrls: ['./public-profile.component.scss']
 })
 export class PublicProfileComponent implements OnInit {
-  userProfileInfo: any; // Adjust as per your User model
+  userProfileInfo: any;
+  userProfileImageUrl: SafeUrl | null = null;
+  favoritedClubs: Club[] = []; // Add this property
 
   constructor(
     private route: ActivatedRoute,
-    private profileService: ProfileService // Use ProfileService
-  ) {
-  }
+    private router: Router,
+    private profileService: ProfileService,
+    private clubService: ClubService, // Inject the ClubService
+    private sanitizer: DomSanitizer
+  ) {}
 
   ngOnInit(): void {
     const userID = this.route.snapshot.paramMap.get('id');
-    console.log('UserID:', userID); // Check if the userID is being retrieved correctly
     this.loadUserProfile(userID);
+    this.loadFavoritedClubs(userID); // Add this call
   }
-
 
   loadUserProfile(userID: string | null) {
     if (userID) {
       this.profileService.getUserProfile(+userID).subscribe(
         (data) => {
-          console.log(data); // Check the structure and data of the userProfileInfo
           this.userProfileInfo = data;
+          this.loadUserProfileImage(userID);
         },
         (error) => {
           console.error('Error fetching user profile', error);
@@ -36,4 +43,36 @@ export class PublicProfileComponent implements OnInit {
       );
     }
   }
+
+  loadUserProfileImage(userID: string) {
+    this.profileService.downloadUserProfilePicture(+userID).subscribe(
+      fileData => {
+        let blob = new Blob([fileData], { type: 'image/jpeg' });
+        let objectURL = URL.createObjectURL(blob);
+        this.userProfileImageUrl = this.sanitizer.bypassSecurityTrustUrl(objectURL);
+      },
+      error => {
+        console.error("Error loading user profile image:", error);
+      }
+    );
+  }
+
+  loadFavoritedClubs(userID: string | null) {
+    if (userID) {
+      this.clubService.getUsersFavoritedClubs(+userID).subscribe(
+        (clubs) => {
+          this.favoritedClubs = clubs;
+        },
+        (error) => {
+          console.error("Error loading favorited clubs:", error);
+        }
+      );
+    }
+  }
+
+  goToClubPage(clubID: number) {
+    this.router.navigate(['/clubs/' + clubID]).then();
+  }
+
+  // You can add goToClubPage method if needed
 }
